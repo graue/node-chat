4f17b0b14f359c770edf019fdade182111b80c5f
Scott Feeney <scott@oceanbase.org>
2013-03-27 01:19:24 -0400

    Add long polling on client side - chat works!! Yay!

--- clientSide.js
+++ clientSide.js
@@ -11,4 +11,33 @@ $(document).ready(function() {
                    color: myColor}
         });
     });
+
+    setTimeout(function() { longPollForMsgsSince(0); }, 10);
 });
+
+function longPollForMsgsSince(time) {
+    $.ajax({
+        url: '/msg-wait?since=' + time,
+        dataType: 'json',
+        error: function(xhr, status, error) {
+            console.log('XHR failed: status=' + status,
+                ', error=' + error);
+            console.log('Retrying in 10sec...');
+            setTimeout(function() { longPollForMsgsSince(time); }, 10000);
+        },
+        success: function(data) {
+            if (!data.messages) {
+                alert('Data is missing messages object! Abort!');
+            } else {
+                data.messages.forEach(function(msg) {
+                    var newMsgHtml = '<p style="color: ' + msg.color +
+                        '">' + msg.text + '</p>';
+                    $('#chat-buffer').append(newMsgHtml);
+                });
+                setTimeout(function() {
+                    longPollForMsgsSince(data.messages.pop().time);
+                }, 0);
+            }
+        }
+    });
+}
